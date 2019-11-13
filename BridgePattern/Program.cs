1b53f11865a0aecd07f4ec5689435cea72a28a3c
jordinola <jordinola@JORDINOLA>
2019-11-13 15:02:12 -0500

    Update master version

--- BridgePattern/Program.cs
+++ BridgePattern/Program.cs
@@ -12,16 +12,19 @@ namespace BridgePattern
             var exception = new CustomException().GetException();
 
             var mongoLogger = new MongoDbLogger(new SpecificMessage());
+            Console.WriteLine("Mongo Logger");
             mongoLogger.Write(exception);
 
             Console.WriteLine("\n=================================================\n");
 
             var sqlLogger = new SqlLogger(new DetailedMessage());
+            Console.WriteLine("SQL Logger");
             sqlLogger.Write(exception);
 
             Console.WriteLine("\n=================================================\n");
 
             var textFileLogger = new TextFileLogger(new CustomMessage());
+            Console.WriteLine("Text File Logger");
             textFileLogger.Write(exception);
 
             Console.ReadLine();
