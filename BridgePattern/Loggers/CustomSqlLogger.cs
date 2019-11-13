9a8b3ce200a893c897b1e020c52d928d5954d23d
jordinola <jordinola@JORDINOLA>
2019-11-13 16:34:52 -0500

    temp

---
+++ BridgePattern/Loggers/CustomSqlLogger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern.Loggers
+{
+    public class CustomSqlLogger : SqlLogger
+    {
+        //TODO...
+    }
+}
