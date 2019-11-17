f4ec22ac82e4534e6f6d03c806b247cd4d7e8991
jjob3011 <jjob3011@gmail.com>
2019-11-17 10:16:35 -0500

    Added tests

---
+++ BridgePattern.Tests/Loggers/DefaultMongoDbLoggerTests.cs
@@ -0,0 +1,44 @@
+using BridgePattern.Data;
+using BridgePattern.Loggers;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+
+namespace Tests.Loggers
+{
+    public class DefaultMongoDbLoggerTests
+    {
+        private DefaultMongoDbLogger logger;
+        private Exception ex;
+
+        [SetUp]
+        public void Setup()
+        {
+            logger = new DefaultMongoDbLogger();
+            ex = new CustomException().GetException();
+        }
+
+        [Test]
+        public void GetErrorMeessage()
+        {
+            var errorMessage = logger.GetErrorMessage(ex);
+            errorMessage.Should().Be($"A default way to log an error in date: { DateTime.Now } with message { ex.Message }");
+        }
+
+        [Test]
+        public void LogToMongoDbTest_Success()
+        {
+            // A test to save a default error message to mongo that should be successfull
+            var success = true;
+            success.Should().BeTrue();
+        }
+
+        [Test]
+        public void LogToMongoDbTest_Error()
+        {
+            // A test to save a default error message to mongo that should fail
+            var success = false;
+            success.Should().BeFalse();
+        }
+    }
+}
