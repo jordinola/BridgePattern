6c92bd2d982e262b7df76b8d7f1600b1fa32401f
jjob3011 <jjob3011@gmail.com>
2019-11-17 09:12:56 -0500

    Added new default additional clases

---
+++ BridgePattern/Loggers/DefaultMongoDbLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgePattern.Loggers
+{
+    public class DefaultMongoDbLogger : ILogger
+    {
+        public void Write(Exception ex)
+        {
+            // MongoDb write logic goes here
+            Console.WriteLine($"A default way to log an error in date: { DateTime.Now } with message { ex.Message }");
+        }
+    }
+}
---
+++ BridgePattern/Loggers/DetailedSqlLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BridgePattern.Loggers
+{
+    public class DetailedSqlLogger : ILogger
+    {
+        public void Write(Exception ex)
+        {
+            // SQL write logic goes here
+            Console.WriteLine($"A more detailed exception ocurred in date: { DateTime.Now }. StackTrace: { ex.StackTrace } InnerException: { ex.InnerException } Message: { ex.Message }");
+        }
+    }
+}
